c93e4b1bc7ddf8076a5cf4ddac148f131899ce80
xiaoying wen <xiaoying wen>
2017-07-18 07:03:53 -0400

    update to use sequelize orm to handle data CRUD

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,40 +2,47 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
-
+// Requiring our models
+var db = require("../models");
+var Sequelize = require('sequelize');
 // Create all our routes and set up logic within those routes where required.
+
+//load all the burgers upon GET request
 router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    // console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+    db.Burger.findAll({}).then(function(dbBurger) {
+        var hbsObject = {
+            burgers: dbBurger
+        };
+        // console.log(hbsObject);
+        
+        res.render("index", hbsObject);
+    });
 });
 
+//add one burger upon POST request
 router.post("/", function(req, res) {
-  burger.create([
-    "burger_name"
-  ], [
-    req.body.burger_name
-  ], function() {
-    res.redirect("/");
-  });
+    //handle the validation error for the burger name db field
+    db.Burger.create(req.body).catch(Sequelize.ValidationError, function (err) {
+        console.log ("Failed to save the entry.");
+        console.log ("System ERR: " + err);
+    }).then(function(dbBurger) {
+        res.redirect("/");
+    });
 });
 
+//update the burger upon PUT request with an id
 router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+    var condition = "id = " + req.params.id;
+    // console.log("condition", condition);
 
-  // console.log("condition", condition);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/");
-  });
+    db.Burger.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(function(dbBurger) {
+            res.redirect("/");
+        });
 });
 
 // Export routes for server.js to use.
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
-var port = process.env.PORT || 3000;
-
+// Sets up the Express App
+// =============================================================
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Requiring our models for syncing
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -25,4 +29,9 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
