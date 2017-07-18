aaa09fc965aa157fdfb489dbce6f3f4a6a25849d
xiaoying wen <xiaoying wen>
2017-07-18 05:03:21 -0400

    add the code base with burger app made last week

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sequelizedBurger
+A restaurant app lets users input the names of burgers they'd like to eat.  The implementation follows the MVC design pattern; uses Node and sequelize to query and route data, and Handlebars to generate the HTML.
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Steakhouse King');
+INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
+
+-- select * from burgers;
