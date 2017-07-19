8a2adb70ac34745f9ed634de56052a541cceb693
xiaoying wen <xiaoying wen>
2017-07-19 06:49:56 -0400

    update UI

---
+++ public/assets/javascript/burger.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function() {
+    $('.row').each(function(i, elem) {
+        $(elem)
+            .find('.data-item')   // Only children of this row
+            .matchHeight({byRow: false}); // Row detection gets confused so disable it
+    });
+});
