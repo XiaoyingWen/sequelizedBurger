2a99056c944922d3446434d1dc4af81672917fe6
xiaoying wen <xiaoying wen>
2017-07-22 07:29:14 -0400

    the table created with sequelize is Burger instead of burger so update the seed sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ('Hamburger');
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Steakhouse King');
-INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
+INSERT INTO Burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO Burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO Burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO Burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO Burgers (burger_name) VALUES ('Steakhouse King');
+INSERT INTO Burgers (burger_name) VALUES ('Veggie Burger');
 
 -- select * from burgers;
